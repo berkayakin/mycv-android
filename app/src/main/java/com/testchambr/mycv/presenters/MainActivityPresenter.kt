f88b349724a96c17e25ee7a581d8c24e7507384a
Berkay AKIN <siratofficial@gmail.com>
2019-11-16 18:33:57 +0100

    Copyright

--- app/src/main/java/com/testchambr/mycv/presenters/MainActivityPresenter.kt
+++ app/src/main/java/com/testchambr/mycv/presenters/MainActivityPresenter.kt
@@ -1,3 +1,11 @@
+/*
+ * *
+ *  * Created by Berkay AKIN on 11/16/19 6:33 PM
+ *  * Copyright (c) 2019 . All rights reserved.
+ *  * Last modified 11/16/19 6:32 PM
+ *
+ */
+
 package com.testchambr.mycv.presenters
 
 import com.testchambr.mycv.api.ApiClient
